e8473054d2d7e0976a6e6d98da5fad9b86e50e6f
Zhifan Zhu <ktw361@gmail.com>
2026-08-16 23:11:37 +0800

    ex04 pass

--- mini_dist/ddp.py
+++ mini_dist/ddp.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from .collectives import all_reduce_mean
-from .buckets import BucketLayout, TensorSlice
+from .buckets import BucketLayout
 from ._todo import todo
 
 
@@ -35,8 +35,19 @@ class MiniDDP(nn.Module):
 
         def make_hook(syncer, offset):
             def hook(grad):
-                syncer.mark_ready(offset, grad)
+                is_last, last_grad = syncer.mark_ready(offset, grad)
+                if is_last:
+                    return last_grad
             return hook
+        
+        def register_params(named_params, sync_id):
+            params = [p for (n, p) in named_params]
+            bucket = BucketLayout.from_tensors(params)
+            syncer = BucketSyncer(bucket, named_params, self, sync_id)
+            for s, param in zip(bucket.slices, params):
+                offset = s.offset
+                param.register_hook(make_hook(syncer, offset))
+            return syncer
 
         # Q: shouldn't we call async_op=True here? and in ex01?
         named_params = []
@@ -45,73 +56,80 @@ class MiniDDP(nn.Module):
             if not parameter.requires_grad:
                 continue
             numel = parameter.numel()
-            if cum_numel + numel >= bucket_cap_numel:
-                params = [p for (n, p) in named_params]
-                bucket = BucketLayout.from_tensors(params)
-                syncer = BucketSyncer(bucket, named_params, self)
-                for s, param in zip(bucket.slices, params):
-                    offset = s.offset
-                    param.register_hook(make_hook(syncer, offset))
+            if cum_numel + numel > bucket_cap_numel:  # flush existing
+                syncer = register_params(named_params, len(self._handles))
                 self._handles.append(syncer)
+                cum_numel = 0
                 named_params = []
-            else:
-                cum_numel += numel
-                named_params.append((name, parameter))
+
+            cum_numel += numel
+            named_params.append((name, parameter))
         
         # register the rest bucket
         if len(named_params) > 0:
-            params = [p for (n, p) in named_params]
-            bucket = BucketLayout.from_tensors(params)
-            syncer = BucketSyncer(bucket, named_params, self)
-            for s, param in zip(bucket.slices, params):
-                offset = s.offset
-                def hook(grad):
-                    syncer.mark_ready(offset, grad)
-                param.register_hook(hook)
+            syncer = register_params(named_params, len(self._handles))
             self._handles.append(syncer)
-        for syncer in self._handles:
-            print(syncer.bucket.total_numel)
-        print(self._handles)
 
         
 class BucketSyncer:
-    def __init__(self, bucket: BucketLayout, params: list, ddp: MiniDDP):
-        """ params are assumed to be correctly offset in bucket """
+    def __init__(self, bucket: BucketLayout, named_params: list, ddp: MiniDDP,
+                sync_id):
+        """ params are assumed to be correctly offset in bucket.
+        The name in named_params, and sync_id is used for potential debugging.
+        The essential ones are `bucket` and `params`
+        """
+        self.sync_id = sync_id
         self.bucket = bucket
         self.all_offsets = set(s.offset for s in bucket.slices)
         self.params = dict()
-        for s, param in zip(self.bucket.slices, params):
-            self.params[s.offset] = param
+        for s, (n, p) in zip(self.bucket.slices, named_params):
+            self.params[s.offset] = (n, p)  # (name, param)
         self.grads = dict()
         self.ddp = ddp  # to increment launcher
     def mark_ready(self, offset, grad):
         # this will be called inside a hook
         self.grads[offset] = grad
-        print('ready', self.grads.keys(), self.all_offsets)
         if len(self.grads) == len(self.all_offsets) \
             and set(self.grads) == self.all_offsets:
-            self.sync()
-    def sync(self):
-        print("Sync called")
-        for n, p in self.params.values():
-            print(f"Before sync, {n}.grad = {p.grad}")
-
+            last_grad = self.sync(last_offset=offset)
+            return True, last_grad
+        else:
+            return False, None  # is_last = False, last_grad = None
+    def sync(self, last_offset: int):
         # sync and clear ready bits
         grad_tensors = []
         for s in self.bucket.slices:
             grad_tensors.append( self.grads[s.offset] )
         mean_grad_tensors = self.bucket.pack(grad_tensors)
         all_reduce_mean(mean_grad_tensors)
-        self.grads.clear()
         views = self.bucket.unpack_views(mean_grad_tensors)
+        last_grad = None
         for s, tensor_view in zip(self.bucket.slices, views):
-            # self.params[s.offset].grad.copy_(tensor_view)
-            n, p = self.params[s.offset]
-            if p.grad is None:
-                print(f"{n=} does not need grad?")
+            if s.offset == last_offset:
+                last_grad = tensor_view
             else:
                 self.params[s.offset][1].grad.copy_(tensor_view)
-        self.ddp.num_bucket_allreduces += 1
 
-        for n, p in self.params.values():
-            print(f"After sync, {n}.grad = {p.grad}")
+        self.grads.clear()
+        self.ddp.num_bucket_allreduces += 1
+        return last_grad
+
+
+"""
+Okay this feels like a crazy exercise...and a software engineering exercise.
+<del>
+when a param is the last tensor to trigger bucket sync(), 
+inside sync() its param.grad will be None, because sync() is itself inside hook, 
+while when pytorch enters the hook, it sets param.grad to None temporarily?
+But param.grad is None is also an indicator that this is the last tensor in the bucket,
+so we can explicitly ask hook to return grad; for other tensors, we set by param.grad.copy_
+</del>
+
+EDIT: above is not fully correct.
+The best way to track the last_grad is simply record the offset that trigger's sync()
+
+Final words:
+    This is a blocking bucket sync.
+    production code will include like async_op, wait etc (instead of calling my ex01 code) 
+        to make sure real comm-backward overlap.
+"""
